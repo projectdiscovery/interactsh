d581434954857ac2ac367d9daddd34ae10804a1b
sandeep <8293321+ehsandeep@users.noreply.github.com>
2021-05-06 20:56:23 +0530

    minor update

--- pkg/server/http_server.go
+++ pkg/server/http_server.go
@@ -114,7 +114,7 @@ const banner = `<h1> Interactsh Server </h1>
 
 <a href='https://github.com/projectdiscovery/interactsh'>Interact.sh </a> is an <b>open-source solution</b> for out-of-band data extraction. It is a tool designed to detect bugs that cause external interactions. These bugs include, Blind SQLi, Blind CMDi, SSRF, etc. <br><br>
 
-If you find communications or exchanges with the %s server in your logs, it is possible that someone has been testing your applications.<br><br>
+If you find communications or exchanges with the <b>%s</b> server in your logs, it is possible that someone has been testing your applications.<br><br>
 
 You should review the time when these interactions were initiated to identify the person responsible for this testing.
 `
