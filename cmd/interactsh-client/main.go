a9ebd02f2fa3e83386d8b4b89fa22b1248a3c550
mzack <marco.rivoli.nvh@gmail.com>
2021-08-09 11:32:22 +0200

    Bumping version to 0.0.4

--- cmd/interactsh-client/main.go
+++ cmd/interactsh-client/main.go
@@ -26,7 +26,6 @@ var (
 	httpOnly     = flag.Bool("http-only", false, "Display only http requests in verbose output")
 	smtpOnly     = flag.Bool("smtp-only", false, "Display smtp interactions")
 	token        = flag.String("token", "", "Authentication token for the server")
-
 )
 
 const banner = `
@@ -34,7 +33,7 @@ const banner = `
    (_)___  / /____  _________ ______/ /______/ /_ 
   / / __ \/ __/ _ \/ ___/ __ '/ ___/ __/ ___/ __ \
  / / / / / /_/  __/ /  / /_/ / /__/ /_(__  ) / / /
-/_/_/ /_/\__/\___/_/   \__,_/\___/\__/____/_/ /_/ v0.0.3
+/_/_/ /_/\__/\___/_/   \__,_/\___/\__/____/_/ /_/ v0.0.4
 `
 
 func showBanner() {
