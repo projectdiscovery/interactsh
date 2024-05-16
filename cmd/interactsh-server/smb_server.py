9eb1b952df44646cad9adfeed3be2459ea97ce82
Mzack9999 <mzack9999@protonmail.com>
2024-05-16 11:19:18 +0200

    removing shares

--- cmd/interactsh-server/smb_server.py
+++ cmd/interactsh-server/smb_server.py
@@ -1,11 +1,10 @@
 import sys
 from impacket import smbserver
 
-def configure_shares(server, share_path):
+def configure_shares(server):
     shares = ["IPC$", "ADMIN$", "C$", "PRINT$", "FAX$", "NETLOGON", "SYSVOL"]
     for share in shares:
         server.removeShare(share)
-        server.addShare(share, share_path)
 
 log_filename = "log.txt"
 if len(sys.argv) >= 2:
@@ -16,9 +15,7 @@ if len(sys.argv) >= 3:
 
 server = smbserver.SimpleSMBServer(listenAddress="0.0.0.0", listenPort=port)
 server.setSMB2Support(True)
-server.addShare("interactsh", "/interactsh")
-
-configure_shares(server, "/interactsh")
+configure_shares(server)
 server.setSMBChallenge('')
 server.setLogFile(log_filename)
 server.start()
