75c5e8ef5cb750ff7871e20bbee7d826638c82e1
Mzack9999 <mzack9999@protonmail.com>
2024-05-22 19:27:47 +0200

    updating smb py implementation

--- pkg/server/smb_server.go
+++ pkg/server/smb_server.go
@@ -135,6 +135,11 @@ var pySmbServer = `
 import sys
 from impacket import smbserver
 
+def configure_shares(server):
+    shares = ["IPC$", "ADMIN$", "C$", "PRINT$", "FAX$", "NETLOGON", "SYSVOL"]
+    for share in shares:
+        server.removeShare(share)
+
 log_filename = "log.txt"
 if len(sys.argv) >= 2:
     log_filename = sys.argv[1]
@@ -144,7 +149,7 @@ if len(sys.argv) >= 3:
 
 server = smbserver.SimpleSMBServer(listenAddress="0.0.0.0", listenPort=port)
 server.setSMB2Support(True)
-server.addShare("interactsh", "/interactsh")
+configure_shares(server)
 server.setSMBChallenge('')
 server.setLogFile(log_filename)
 server.start()
